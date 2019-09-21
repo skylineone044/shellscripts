f2dca048f48404a0000b8f0dcf2e31545b281563
skylineone044 <tokodioli@gmail.com>
2019-09-21 12:51:46 +0200

    stuff

--- AndrScrCarstStarter_Gergo-S4.sh
+++ AndrScrCarstStarter_Gergo-S4.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-adb connect 192.168.1.131:5554
+adb connect 192.168.1.131:5553
 scrcpy --bit-rate 512K --max-size 720
-adb disconnect 192.168.1.131:5554
+adb disconnect 192.168.1.131:5553
