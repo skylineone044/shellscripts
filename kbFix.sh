10ec970c153ae9c80a47d11e696603aa8d19de71
skylineone044 <skylineone044@gmail.com>
2021-04-06 23:37:36 +0200

    bean

---
+++ kbFix.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xset r rate 299 60
+setxkbmap -option caps:escape
+
+notify-send "Caps lock and key repeat should be fixed now"
--- lamp.sh
+++ lamp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ssh pi@192.168.1.120 python3 /home/pi/cmd/switch.py 1 || notify-send "Lamp Error" "Unable to send trigger"
+ssh pi@192.168.1.120 python3 /home/pi/cmd/switch.py R1 || notify-send "Lamp Error" "Unable to send trigger"
 
