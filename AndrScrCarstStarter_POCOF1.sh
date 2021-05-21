74008966dd79a8991c6ee9b9962a054b3d3c850a
skylineone044 <skylineone044@gmail.com>
2021-05-21 13:56:23 +0200

    accept port in arg

--- AndrScrCarstStarter_POCOF1.sh
+++ AndrScrCarstStarter_POCOF1.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-adb connect 192.168.1.124:5555
+if [[ $# -eq 1 ]]; then
+    let port=$1
+else
+    let port=5555
+fi
+
+adb connect 192.168.1.124:$port
 scrcpy --bit-rate 25M --max-size 1080
-adb disconnect 192.168.1.124:5555
+adb disconnect 192.168.1.124:$port
