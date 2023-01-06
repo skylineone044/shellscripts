37fc436b5829e085941efa68b3a4341a79540ca3
skylineone044 <skylineone044@gmail.com>
2023-01-06 23:20:13 +0100

    add comments and link for ssh

--- new_user.sh
+++ new_user.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 id $1
 
 if [ $? -eq 0 ]
@@ -7,6 +8,7 @@ then
     echo "error: user $1 exists"
     exit 1
 fi
+
 
 sudo useradd -m $1
 sudo usermod -aG sftp_users $1
@@ -22,12 +24,16 @@ sudo -u $1 ssh-keygen -f "/home/$1/.ssh/$1_sftp_rsa" -t rsa
 echo "key generated"
 echo ""
 
+#copy contents of the pub file into authorized_keys abd then remove it
 sudo -u $1 cat "/home/$1/.ssh/$1_sftp_rsa.pub" | sudo -u $1 tee /home/$1/.ssh/authorized_keys
 sudo rm "/home/$1/.ssh/$1_sftp_rsa.pub"
+#move te private key to desktop
 sudo mv "/home/$1/.ssh/$1_sftp_rsa" /home/hostpc/Desktop
 sudo chmod 644 /home/$1/.ssh/authorized_keys
 echo "authorization set up"
 echo ""
 
 sudo mv "/home/$1" "/mnt/mass/sftp"
+#linking the home dir to the server's home for the ssh daemon to find the pub key
+sudo ln -s /mnt/mass/sftp/$1/ /home/$1
 echo "moved home to /mnt/mass/sftp/"
