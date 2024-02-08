7e733a5d0a241305d79ec59154dd18fe4ed5a906
skylineone044 <skylineone044@gmail.com>
2024-02-08 06:46:15 +0100

    remove reudundant scripts

--- fan.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ssh pi python3 /home/pi/cmd/switch.py R7 || notify-send "fan Error" "Unable to send trigger"
-
--- lamp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ssh pi python3 /home/pi/cmd/switch.py R1 || notify-send "Lamp Error" "Unable to send trigger"
-
