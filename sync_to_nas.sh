f6c450d2da4ff89d8a22ee1d78d8f2ed8535b8b8
skylineone044 <skylineone044@gmail.com>
2024-03-24 15:42:50 +0100

    do not hand if notification service is unavailable

--- sync_to_nas.sh
+++ sync_to_nas.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-notify-send "NAS Sync" "Starting: Pictures"
+notify-send "NAS Sync" "Starting: Pictures" &
 rsync -rvhlpt /mnt/cupcake/Pictures skyline@truenas:/mnt/array/media/
 rsync -rvhlpt skyline@truenas:/mnt/array/media/Pictures /mnt/cupcake/
 
-notify-send "NAS Sync" "Starting: Music"
+notify-send "NAS Sync" "Starting: Music" &
 rsync -rvhlpt /mnt/cupcake/Music skyline@truenas:/mnt/array/media/
 
-notify-send "NAS Sync" "Starting: Videos"
+notify-send "NAS Sync" "Starting: Videos" &
 rsync -rvhlpt /mnt/cupcake/Videos skyline@truenas:/mnt/array/media/
 
-notify-send "NAS Sync" "Finished"
+notify-send "NAS Sync" "Finished" &
