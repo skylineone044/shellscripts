76189faa81522f56a3a12fa58a7d4ca337d5969c
skylineone044 <skylineone044@gmail.com>
2024-04-27 11:26:54 +0200

    fix for portrait images

--- downsize_to_4k.sh
+++ downsize_to_4k.sh
@@ -2,4 +2,4 @@
 
 set -o xtrace
 
-find ./ -name "*.png" -type f -exec bash -c 'convert "$1" -resize 3840x2160\> ${1/7680x4320/3840x2160} && trash "$1"' bash {} \;
+find ./ -name "*.png" -type f -exec bash -c 'convert "$1" -resize 8294400@ ${1/7680x4320/3840x2160} && trash "$1"' bash {} \;
