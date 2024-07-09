eceae2e970e56c27796b773b2123e9d133a58095
skylineone044 <skylineone044@gmail.com>
2024-07-09 02:16:39 +0200

    update command

--- downsize_to_4k.sh
+++ downsize_to_4k.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -o xtrace
-find ./ -name "*_7680x4320.png" -type f -print0 | xargs -0 --max-args=1 --max-procs=16 bash -c 'convert "$1" -resize 8294400@ ${1/7680x4320/3840x2160} && trash "$1"' bash
+find ./ -name "*_7680x4320.png" -type f -print0 | xargs -0 --max-args=1 --max-procs=16 bash -c 'magick "$1" -resize 8294400@ ${1/7680x4320/3840x2160} && trash "$1"' bash
